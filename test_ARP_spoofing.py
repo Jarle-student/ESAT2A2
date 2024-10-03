eb1ad2d9cb481a78a41ca9b4311a45a40dcd8862
Jarle-student <jarle.braeken@student.kuleuven.be>
2024-10-03 15:01:19 +0200

    Update test_ARP_spoofing.py

--- test_ARP_spoofing.py
+++ test_ARP_spoofing.py
@@ -1,6 +1,22 @@
 from scapy.all import *
 from scapy.layers.l2 import ARP, Ether
 import time
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', dest='target', help='Target IP Address/Adresses')
+    options = parser.parse_args()
+
+    #Check for errors i.e if the user does not specify the target IP Address
+    #Quit the program if the argument is missing
+    #While quitting also display an error message
+    if not options.target:
+        #Code to handle if interface is not specified
+        parser.error("[-] Please specify an IP Address or Addresses, use --help for more info.")
+    return options
+
+options = get_args()
 
 def get_mac(ip):
     """
