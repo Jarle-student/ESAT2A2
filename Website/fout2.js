ece04b7e7f6efaddadcf43990e0b0441006aa4c5
deepblue16 <arthur.cukier05@gmail.com>
2024-11-13 16:52:14 +0100

    Add files via upload

---
+++ Website/fout2.js
@@ -0,0 +1,4 @@
+// JavaScript to navigate to another HTML page on button click
+document.getElementById('button').addEventListener('click', function() {
+    window.location.href = 'index.html';  // Replace with the desired page
+});
