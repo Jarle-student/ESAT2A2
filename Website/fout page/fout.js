eeacb5dd8a7c57a4ca08dd24d3081cadf115d3fc
deepblue16 <arthur.cukier05@gmail.com>
2024-11-21 16:49:07 +0100

    Update fout.js

--- Website/fout page/fout.js
+++ Website/fout page/fout.js
@@ -1,1 +1,16 @@
+// Get the firstname from the URL query parameter
+const urlParams = new URLSearchParams(window.location.search);
+const firstname = urlParams.get('firstname');
+
+// Select the message element
+const messageElement = document.getElementById('message');
+
+// Update the message based on the presence of the firstname parameter
+if (firstname) {
+    // Display a personalized message if firstname is found
+    messageElement.textContent = `Congratulations, ${firstname}! You've successfully logged in.`;
+} else {
+    // Display a default message if no firstname is found
+    messageElement.textContent = 'Congratulations, you have successfully logged in!';
+}
 
