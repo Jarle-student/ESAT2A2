9fe304cbb4d7ff19164ded5b2dd21f7cc055f7da
deepblue16 <arthur.cukier05@gmail.com>
2024-11-21 16:52:01 +0100

    Delete Website/fout2.js

--- Website/fout2.js
+++
@@ -1,6 +0,0 @@
-// JavaScript to navigate to another HTML page on button click
-document.getElementById('button').addEventListener('click', function() {
-    // Dynamically construct the URL for navigation
-    const baseUrl = window.location.origin;
-    window.location.href = `${baseUrl}/index.html`; // Replace with the desired page if different
-});
