329ed6e1ce5c606dba50d44fe9d0919675ae1e94
willemhendig <willem.hendig@student.kuleuven.be>
2024-12-09 15:39:02 +0000

    Update wep_sniffer_v2.py

--- Sniffer/wep_sniffer_v2.py
+++ Sniffer/wep_sniffer_v2.py
@@ -1,16 +1,18 @@
 from scapy.all import *
 from RC4_decryption import bin_list_to_decimal_list, hex_to_8_bin_list, extend_key, generate_keystream_8_bin, decrypt
 """
-Vergeet niet
+Don't forget
 sudo iwconfig wlan0mon channel 1
 """
 
+### Filter for Dot11 and MAC address
 def wep_filter(packet):
     bssid = "B8:27:EB:A7:6C:47"
     if packet.haslayer(Dot11):
         return (packet.addr1 == bssid.lower() or packet.addr2 == bssid.lower() or packet.addr3 == bssid.lower())
     return False
 
+### Decrypt the wepdata
 def decrypt_packet(wepdata, iv, WEP_key):
     key_hex = iv.hex() + WEP_key.encode("utf-8").hex()
     key_ascii_list = bin_list_to_decimal_list(hex_to_8_bin_list(key_hex))
@@ -20,16 +22,20 @@ def decrypt_packet(wepdata, iv, WEP_key):
     decrypted_packet = decrypt(keystream, wepdata_8_bin_list)
     return decrypted_packet
 
+### Process the packet
 def packet_handler(packet, wep_key):
+    ### WEP encryption
     if packet.haslayer(Dot11WEP):
         iv = packet[Dot11WEP].iv
         enc_data = packet[Dot11WEP].wepdata
         decr_data = decrypt_packet(enc_data, iv, wep_key)
         print(f"Decrypted data: {decr_data}")
 
+    ### No WEP encryption, only Dot11
     else:
         print("No WEP-encryption")
 
+### Start the sniffer
 def start_sniffer(interface, wep_key):
     print(f"Sniffing on interface: {interface}")
     try:
